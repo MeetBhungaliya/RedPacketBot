672929676c2e482288cb7ed6f34a4ca4d30fbd56
meet-andro <meet.andropedia@gmail.com>
2025-02-25 14:44:31 +0530

    env config

--- src/lib/envconfig.ts
+++ src/lib/envconfig.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import path from 'path'
 
-const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env'
+const envFile = process.env.NODE_ENV === 'production' ? '.env' : '.env.development'
 
 if (dotenv.config({ path: path.resolve(process.cwd(), envFile) }).error) {
   console.error('No environment variable found')
